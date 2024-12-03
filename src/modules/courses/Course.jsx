3e967dd8b28d886eb6eb54cef05cf3ddd811a0a0
Barkhasbadi Ankhbileg <barkhasbadi.a@mbank.local>
2024-12-03 09:00:39 +0800

    Part 2 done

--- src/modules/courses/Course.jsx
+++ src/modules/courses/Course.jsx
@@ -5,7 +5,7 @@ import Total from "./Total";
 const Course = ({ course }) => {
   return (
     <div>
-      <Header name={course.name} />
+      <Header name={course.name} hsize={'h2'}/>
       <Content courses={course.parts} />
       <Total parts={course.parts} />
     </div>
